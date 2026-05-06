be11e82089d301083121dc86da6414b5710d9616
Ankitha2807 <knankitha73@gmail.com>
2026-05-06 17:22:12 +0530

    Point frontend to deployed Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://job-application-tracker-zlen.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
